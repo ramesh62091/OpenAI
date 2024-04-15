451a18df19323970315e08fd1ac7368f0ab341c5
ramesh62091 <167061197+ramesh62091@users.noreply.github.com>
2024-04-15 13:02:12 +0530

    Add files via upload

---
+++ Controllers/IConfigProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace OpenAIChatGPT.Controllers
+{
+    public interface IConfigProvider
+    {
+        string GetAPIKey { get; }
+    }
+
+    public class ConfigProvider : IConfigProvider
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigProvider(IConfiguration configProvider)
+        {
+            _configuration = configProvider;
+        }
+
+        public string GetAPIKey => GetValue<string>("Authentication:OpenAIAPIKey");
+        private T GetValue<T>(string key)
+        {
+            return _configuration.GetValue<T>(key) ?? throw new KeyNotFoundException($"{key} not found!");
+        }
+    }
+}
---
+++ Controllers/OpenAI.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace OpenAIChatGPT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAI : ControllerBase
+    {
+        private readonly IConfigProvider _iconfigprovider;
+        public OpenAI(IConfigProvider iconfigprovider)
+        {
+            _iconfigprovider = iconfigprovider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetData(string input)
+        {
+            string apiKey = _iconfigprovider.GetAPIKey;
+            string response = "";
+            OpenAIAPI openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = input;
+            completion.Model = "gpt-3.5-turbo-1106";
+            completion.MaxTokens = 4000;
+            var output = await openai.Completions.CreateCompletionAsync(completion);
+            if (output != null)
+            {
+                foreach (var item in output.Completions)
+                {
+                    response = item.Text;
+                }
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest($"Dat not found");
+            }
+        }
+    }
+}
